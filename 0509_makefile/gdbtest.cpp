bb3ba40d2e98a3c4348c1c460e802ae2a926aca3
ubuntu <liyubo_liyubo@163.com>
2017-05-09 08:26:46 -0700

    0509

---
+++ 0509_makefile/gdbtest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arry[10]={1,2,3,4,9,8,7,6,5,10};
+	for(int i=0;i<10;i++)
+	{
+		cout<<arry[i];
+	}
+
+return 0;
+
+
+}
--- 0509_makefile/makefile/main.c
+++ 0509_makefile/makefile/main.c
@@ -1,10 +1,10 @@
 #include<stdio.h> 
-#include<test.h>
+#include"test.h"
 int main(void )
 {
-	int  a=0;
+	int a=0;
 	int b=0;
-	b=sum(1,2);
+	b=add(1,2);
 	printf("%d\n",b);
 	return 0;
 }
