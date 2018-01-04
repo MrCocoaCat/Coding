adeb122de910fa5790408b8cc95ee082ee563cc0
liyubo <liyubo_liyubo@163.com>
2018-01-04 16:18:39 +0800

    add LearnCmake

---
+++ LearnCmake/t1/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello World from ti1 Main\n");
+	return 0;
+}
+
---
+++ LearnCmake/t3/lib/hello.c
@@ -0,0 +1,6 @@
+#include"hello.h"
+void HelloFunc()
+{
+	printf("Hello World from ti1 Main\n");
+}
+
