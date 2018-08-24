219433cae8ebca385fdb0f993d22eeff26fe8d47
MrCocoaCat <liyubo_liyubo@163.com>
2018-08-24 21:29:07 +0800

    add opp.py
    Signed-off-by: MrCocoaCat <liyubo_liyubo@163.com>

---
+++ LearnPython/oop.py
@@ -0,0 +1,18 @@
+# 定义一个空类
+class Student():
+    pass
+
+
+class PythonStudent():
+    name = None
+    age = 18
+
+    def myfun(self):
+        print("do homework")
+        return None
+
+wang = PythonStudent()
+print(wang.age)
+
+print(PythonStudent.__dict__)
+
