5d7c73276ca2f3b57e7978513d7945241f758370
MrCocoaCat <liyubo_liyubo@163.com>
2018-08-28 13:18:42 +0800

    modify oop3.py
    Signed-off-by: MrCocoaCat <liyubo_liyubo@163.com>

--- LearnPython/oop3.py
+++ LearnPython/oop3.py
@@ -11,8 +11,24 @@ class B(A):
 
 # 多继承，可以使用父类的功能
 class C(B):
+    age = "sf"
     pass
 
 
 c = C()
+print(C.__mro__)
+# 多态
+# 多态不是语法，是一种设计思想
 
+# 检测是否为子类
+print(issubclass(B, A))
+
+# 检测是否未其实例
+print(isinstance(c, C))
+
+# 检测是否含有某个成员,检测aeg是否为c的成员
+print(hasattr(c, "age"))
+
+# 获取函数成员列表
+
+print(dir(c))
