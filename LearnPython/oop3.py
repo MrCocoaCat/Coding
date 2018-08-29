0488c7ee2e9afbdeaec36cd9c8f447e84c0cb740
MrCocoaCat <liyubo_liyubo@163.com>
2018-08-29 20:57:53 +0800

    modify oop3.py
    Signed-off-by: MrCocoaCat <liyubo_liyubo@163.com>

--- LearnPython/oop3.py
+++ LearnPython/oop3.py
@@ -32,3 +32,43 @@ print(hasattr(c, "age"))
 # 获取函数成员列表
 
 print(dir(c))
+
+# get　获取属性
+# set　添加属性
+# delete 删除属性
+
+class Student():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.setname(self.name)
+    # 介绍自己
+    def intro(self):
+        print("Hi my name is {0}".format(self.name))
+
+    def setname(self, name):
+        self.name = name.upper()
+
+
+s1 = Student("aa", 18)
+s1.intro()
+
+class Person():
+    def fget(self):
+        return self._name
+
+    def fset(self, name):
+        self._name = name.upper()
+
+    name = property(fget, fset, "do pro")
+
+p = Person()
+p.name = "tulin"
+print(p.name)
+
+
+# __dict__ 以字典方式显示类的成员组成
+# __doc__ 获取类的文档信息　
+# __name__ 获取类的名称，如果在模块中使用，则获取模块名称
+# __base__　获取某个类的所有父类
+
